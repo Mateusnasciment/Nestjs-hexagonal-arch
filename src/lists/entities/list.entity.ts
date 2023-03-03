922eb86e768c9fb402f91f1001c877201a4c89cb
mateusnasciment <mateus.nasciment@hotmail.com>
2023-03-03 17:46:33 -0300

    feat: table list

---
+++ src/lists/entities/list.entity.ts
@@ -0,0 +1,14 @@
+import { table } from "console"
+
+
+@Table("lists")
+export class List {
+    name: 'List',
+    properties: {
+      id: { type: 'string', id: true },
+      name: { type: 'string' },
+      description: { type: 'string' },
+      createdAt: { type: 'datetime' },
+      updatedAt: { type: 'datetime' },
+    },
+}
